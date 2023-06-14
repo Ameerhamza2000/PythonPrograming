590855c438ffebc0da0d1da72cf0d4db4030a8aa
Ameer Hamza <ameer692hamza@gmail.com>
2023-06-14 10:23:30 +0500

    changes

---
+++ builtInFunctions.py
@@ -0,0 +1,18 @@
+# print()
+print('Enter your name :')
+
+# input() always take input as string if you enter a number then conversion is required
+myName = input()
+
+print('Enter your age :')
+myAge = int(input())
+
+# type()
+print(type(myName))
+print(type(myAge))
+
+# round() to round a number
+x=round(5.42314)
+print(x)
+y = round(5.55646,2)
+print(y)

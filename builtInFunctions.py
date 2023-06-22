a6340b2ca1c8b2e92a2d12eceda0bd55eddff2b6
Ameer Hamza <ameer692hamza@gmail.com>
2023-06-22 10:32:22 +0500

    builtin update

--- builtInFunctions.py
+++ builtInFunctions.py
@@ -16,3 +16,11 @@ x=round(5.42314)
 print(x)
 y = round(5.55646,2)
 print(y)
+
+# absolute value which converts negative values into positive like lengths which are never negative
+length1= -18.5
+print(abs(length1))
+
+# power of something
+number=2
+print(pow(number,4))
